d25623883ca3423a760d600246f7cc3cea555bdc
Larry Lindsey <llindsey@clm.utexas.edu>
2013-09-20 15:53:02 -0500

    BlockMatchCallable stub

---
+++ TrakEM2_/src/main/java/mpicbg/trakem2/align/concurrent/BlockMatchCallable.java
@@ -0,0 +1,18 @@
+package mpicbg.trakem2.align.concurrent;
+
+import java.io.Serializable;
+import java.util.concurrent.Callable;
+
+/**
+ *
+ */
+public class BlockMatchCallable implements Callable, Serializable
+{
+
+
+    @Override
+    public Object call() throws Exception
+    {
+        return null;
+    }
+}
