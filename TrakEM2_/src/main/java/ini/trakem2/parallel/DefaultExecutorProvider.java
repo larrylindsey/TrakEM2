09ef05eced5bb49e8492169d01dccb49b2dabf4d
Larry Lindsey <llindsey@clm.utexas.edu>
2013-09-24 12:17:53 -0500

    Serialization and support for working with FijiArchipelago

---
+++ TrakEM2_/src/main/java/ini/trakem2/parallel/DefaultExecutorProvider.java
@@ -0,0 +1,25 @@
+package ini.trakem2.parallel;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Default Executor Provider, which creates ExecutorServices from java.util.concurrent.Executors
+ *
+ * @author Larry Lindsey
+ */
+public class DefaultExecutorProvider extends ExecutorProvider
+{
+
+    public ExecutorService getService(int nThreads)
+    {
+        return Executors.newFixedThreadPool(nThreads);
+    }
+
+    public ExecutorService getService(float fractionThreads)
+    {
+        int nc = (int)(fractionThreads *
+                (float)Runtime.getRuntime().availableProcessors());
+        return Executors.newFixedThreadPool(nc > 0 ? nc : 1);
+    }
+}
